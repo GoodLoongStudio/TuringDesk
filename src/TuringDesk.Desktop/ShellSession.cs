1b4e49ec3a664b3da30d3ee6ab67a0aeb3bec6f5
GoodLoongStudio <chnmlong@hotmail.com>
2026-08-17 01:06:24 +0800

    feat: track desktop enhancement mode

--- src/TuringDesk.Desktop/ShellSession.cs
+++ src/TuringDesk.Desktop/ShellSession.cs
@@ -3,5 +3,6 @@ namespace TuringDesk.Desktop;
 internal static class ShellSession
 {
     public static bool IsShellMode { get; set; }
+    public static bool IsEnhancementMode { get; set; }
     public static bool ExitRequested { get; set; }
 }
