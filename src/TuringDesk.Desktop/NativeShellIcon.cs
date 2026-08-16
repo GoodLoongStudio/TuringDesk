28ee61e94b964767297e4e595dcf7e13891432a8
GoodLoongStudio <chnmlong@hotmail.com>
2026-08-16 18:00:55 +0800

    feat: add reusable native Windows icon control

---
+++ src/TuringDesk.Desktop/NativeShellIcon.cs
@@ -0,0 +1,49 @@
+using System.Windows;
+using System.Windows.Controls;
+using TuringDesk.Desktop.Services;
+
+namespace TuringDesk.Desktop;
+
+public sealed class NativeShellIcon : Image
+{
+    public static readonly DependencyProperty StockIconProperty = DependencyProperty.Register(
+        nameof(StockIcon),
+        typeof(ShellStockIconId),
+        typeof(NativeShellIcon),
+        new PropertyMetadata(ShellStockIconId.Application, OnIconPropertyChanged));
+
+    public static readonly DependencyProperty LargeProperty = DependencyProperty.Register(
+        nameof(Large),
+        typeof(bool),
+        typeof(NativeShellIcon),
+        new PropertyMetadata(false, OnIconPropertyChanged));
+
+    public ShellStockIconId StockIcon
+    {
+        get => (ShellStockIconId)GetValue(StockIconProperty);
+        set => SetValue(StockIconProperty, value);
+    }
+
+    public bool Large
+    {
+        get => (bool)GetValue(LargeProperty);
+        set => SetValue(LargeProperty, value);
+    }
+
+    public NativeShellIcon()
+    {
+        Stretch = System.Windows.Media.Stretch.Uniform;
+        RenderOptions.SetBitmapScalingMode(this, System.Windows.Media.BitmapScalingMode.HighQuality);
+        Loaded += (_, _) => RefreshIcon();
+    }
+
+    private static void OnIconPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is NativeShellIcon icon) icon.RefreshIcon();
+    }
+
+    private void RefreshIcon()
+    {
+        Source = ShellIconService.GetStockIcon(StockIcon, Large);
+    }
+}
