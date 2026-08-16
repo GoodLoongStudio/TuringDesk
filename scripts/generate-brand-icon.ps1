33bbfcbbe3be7a6385f823b317f3c9f47aa308fe
GoodLoongStudio <chnmlong@hotmail.com>
2026-08-16 17:58:29 +0800

    build: generate multi-size TuringDesk application icon

---
+++ scripts/generate-brand-icon.ps1
@@ -0,0 +1,130 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$OutputPath
+)
+
+$ErrorActionPreference = "Stop"
+Add-Type -AssemblyName System.Drawing
+
+function New-TuringDeskPng([int]$Size) {
+    $bitmap = New-Object System.Drawing.Bitmap($Size, $Size, [System.Drawing.Imaging.PixelFormat]::Format32bppArgb)
+    $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+    try {
+        $graphics.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
+        $graphics.Clear([System.Drawing.Color]::Transparent)
+
+        $scale = $Size / 256.0
+        function S([double]$v) { return [single]($v * $scale) }
+
+        $badgeBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(255, 18, 39, 78))
+        $ringPen = New-Object System.Drawing.Pen([System.Drawing.Color]::FromArgb(255, 49, 104, 212), (S 7))
+        $innerPen = New-Object System.Drawing.Pen([System.Drawing.Color]::FromArgb(190, 32, 72, 151), (S 4))
+        $arrowBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(255, 58, 116, 255))
+        $highlightBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(255, 92, 148, 255))
+        $glintBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(220, 120, 176, 255))
+
+        try {
+            $graphics.FillEllipse($badgeBrush, (S 30), (S 30), (S 196), (S 196))
+            $graphics.DrawEllipse($ringPen, (S 30), (S 30), (S 196), (S 196))
+            $graphics.DrawEllipse($innerPen, (S 47), (S 47), (S 162), (S 162))
+
+            $arrow = @(
+                [System.Drawing.PointF]::new((S 106), (S 73)),
+                [System.Drawing.PointF]::new((S 168), (S 73)),
+                [System.Drawing.PointF]::new((S 168), (S 56)),
+                [System.Drawing.PointF]::new((S 208), (S 96)),
+                [System.Drawing.PointF]::new((S 168), (S 136)),
+                [System.Drawing.PointF]::new((S 168), (S 117)),
+                [System.Drawing.PointF]::new((S 133), (S 117)),
+                [System.Drawing.PointF]::new((S 150), (S 134)),
+                [System.Drawing.PointF]::new((S 125), (S 159)),
+                [System.Drawing.PointF]::new((S 76), (S 110)),
+                [System.Drawing.PointF]::new((S 101), (S 85)),
+                [System.Drawing.PointF]::new((S 125), (S 109)),
+                [System.Drawing.PointF]::new((S 145), (S 89)),
+                [System.Drawing.PointF]::new((S 106), (S 89))
+            )
+            $graphics.FillPolygon($arrowBrush, $arrow)
+
+            $highlight = @(
+                [System.Drawing.PointF]::new((S 168), (S 61)),
+                [System.Drawing.PointF]::new((S 200), (S 96)),
+                [System.Drawing.PointF]::new((S 168), (S 127)),
+                [System.Drawing.PointF]::new((S 168), (S 114)),
+                [System.Drawing.PointF]::new((S 151), (S 114)),
+                [System.Drawing.PointF]::new((S 160), (S 104)),
+                [System.Drawing.PointF]::new((S 176), (S 104)),
+                [System.Drawing.PointF]::new((S 185), (S 96)),
+                [System.Drawing.PointF]::new((S 176), (S 87)),
+                [System.Drawing.PointF]::new((S 160), (S 87)),
+                [System.Drawing.PointF]::new((S 151), (S 78)),
+                [System.Drawing.PointF]::new((S 168), (S 78))
+            )
+            $graphics.FillPolygon($highlightBrush, $highlight)
+            $graphics.FillEllipse($glintBrush, (S 114), (S 93), (S 12), (S 12))
+        }
+        finally {
+            $badgeBrush.Dispose()
+            $ringPen.Dispose()
+            $innerPen.Dispose()
+            $arrowBrush.Dispose()
+            $highlightBrush.Dispose()
+            $glintBrush.Dispose()
+        }
+
+        $stream = New-Object System.IO.MemoryStream
+        $bitmap.Save($stream, [System.Drawing.Imaging.ImageFormat]::Png)
+        return $stream.ToArray()
+    }
+    finally {
+        $graphics.Dispose()
+        $bitmap.Dispose()
+    }
+}
+
+$sizes = @(16, 20, 24, 32, 40, 48, 64, 96, 128, 256)
+$images = foreach ($size in $sizes) {
+    [PSCustomObject]@{
+        Size = $size
+        Bytes = New-TuringDeskPng $size
+    }
+}
+
+$target = [System.IO.Path]::GetFullPath($OutputPath)
+$parent = [System.IO.Path]::GetDirectoryName($target)
+if (-not [string]::IsNullOrWhiteSpace($parent)) {
+    [System.IO.Directory]::CreateDirectory($parent) | Out-Null
+}
+
+$stream = [System.IO.File]::Create($target)
+$writer = New-Object System.IO.BinaryWriter($stream)
+try {
+    # ICONDIR: reserved, type=icon, image count.
+    $writer.Write([UInt16]0)
+    $writer.Write([UInt16]1)
+    $writer.Write([UInt16]$images.Count)
+
+    $offset = 6 + (16 * $images.Count)
+    foreach ($image in $images) {
+        $dimension = if ($image.Size -ge 256) { [byte]0 } else { [byte]$image.Size }
+        $writer.Write($dimension)
+        $writer.Write($dimension)
+        $writer.Write([byte]0)
+        $writer.Write([byte]0)
+        $writer.Write([UInt16]1)
+        $writer.Write([UInt16]32)
+        $writer.Write([UInt32]$image.Bytes.Length)
+        $writer.Write([UInt32]$offset)
+        $offset += $image.Bytes.Length
+    }
+
+    foreach ($image in $images) {
+        $writer.Write($image.Bytes)
+    }
+}
+finally {
+    $writer.Dispose()
+    $stream.Dispose()
+}
+
+Write-Host "Generated TuringDesk multi-size icon: $target" -ForegroundColor Green
