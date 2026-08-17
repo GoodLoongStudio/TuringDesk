3d6487dcb0f29499695c20cc8d74b9e4374cc2b5
GoodLoongStudio <chnmlong@hotmail.com>
2026-08-17 10:46:54 +0800

    feat: expose product desktop library window

---
+++ src/TuringDesk.Desktop/MainWindow.DesktopLibrary.cs
@@ -0,0 +1,19 @@
+namespace TuringDesk.Desktop;
+
+public partial class MainWindow
+{
+    internal void ShowDesktopLibrary()
+    {
+        if (!Dispatcher.CheckAccess())
+        {
+            Dispatcher.Invoke(ShowDesktopLibrary);
+            return;
+        }
+
+        var window = new DesktopLibraryWindow(_runtime, _modelStore)
+        {
+            Owner = IsVisible ? this : null
+        };
+        window.ShowDialog();
+    }
+}
