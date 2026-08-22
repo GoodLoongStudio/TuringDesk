a27d60118d71b2d04c315662c73c55211c43088a
GoodLoongStudio <chnmlong@hotmail.com>
2026-08-23 00:42:31 +0800

    harness: add shared model settings bridge

---
+++ src/native/include/turingdesk/HarnessSettingsBridge.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+
+namespace turingdesk {
+
+struct HarnessSettingsBridgeState {
+    bool configured{};
+    bool hasApiKey{};
+    std::wstring providerId;
+    std::wstring model;
+    std::wstring baseUrl;
+    std::wstring protocol;
+    std::wstring dshHome;
+    std::wstring apiKey;
+    std::wstring error;
+};
+
+HarnessSettingsBridgeState PrepareHarnessSettingsBridge();
+bool HarnessSettingsBridgeSelfTest();
+
+} // namespace turingdesk
