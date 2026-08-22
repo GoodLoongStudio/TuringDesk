a56ad11ab56657ecad32c8420eac2d41c543bc02
GoodLoongStudio <chnmlong@hotmail.com>
2026-08-22 19:20:23 +0800

    wallpaper: let library target a specific monitor

--- src/native/include/turingdesk/WallpaperLibraryWindow.h
+++ src/native/include/turingdesk/WallpaperLibraryWindow.h
@@ -3,14 +3,22 @@
 #include <windows.h>
 #include <functional>
 #include <memory>
+#include <string>
+#include <vector>
 
 #include "turingdesk/WallpaperLibrary.h"
 
 namespace turingdesk::wallpaper {
 
+struct WallpaperLibraryTarget {
+    std::wstring monitorId;
+    std::wstring displayName;
+    bool primary{};
+};
+
 class WallpaperLibraryWindow {
 public:
-    using ApplyCallback = std::function<void(const WallpaperLibraryItem&)>;
+    using ApplyCallback = std::function<void(const WallpaperLibraryItem&, const std::wstring& targetMonitorId)>;
 
     WallpaperLibraryWindow();
     ~WallpaperLibraryWindow();
@@ -18,7 +26,10 @@ public:
     WallpaperLibraryWindow(const WallpaperLibraryWindow&) = delete;
     WallpaperLibraryWindow& operator=(const WallpaperLibraryWindow&) = delete;
 
-    bool Show(HINSTANCE instance, WallpaperLibrary* library, ApplyCallback applyCallback);
+    bool Show(HINSTANCE instance, WallpaperLibrary* library,
+              const std::vector<WallpaperLibraryTarget>& targets,
+              ApplyCallback applyCallback);
+    void SetTargets(const std::vector<WallpaperLibraryTarget>& targets);
     void Close();
     void Refresh();
     bool Visible() const noexcept;
